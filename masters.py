53ae24b52adf772ff45864317f5d37d6d197cc10
tritoch <tritochs@gmail.com>
2017-03-02 16:38:38 -0600

    Updated for MM3

--- masters.py
+++ masters.py
@@ -1,5 +1,7 @@
-# masters.py is a script to assist in the handling of masters sets
-# these sets are entirely made of reprints. the only new information
+# -*- coding: utf-8 -*-
+
+# masters.py is a script to assist in the handling of masters sets.
+# These sets are entirely made of reprints. the only new information
 # we care about is their new art and their rarity. WOTC does not include
 # rarity on the spoiler page, so we're going to have to figure out out.
 # The set symbol on current sets is color coded based on rarity.
@@ -9,6 +11,7 @@
 # from the average color of symbols.  Max variance from images with correct
 # set symbol placement is about *7*, we set the alarm rate at 10.
 
+import io
 from lxml import html
 import requests
 import urllib
@@ -17,15 +20,15 @@ from PIL import Image
 import json
 import time
 
-setname = 'EMA'
+setname = 'MM3'
 
 # we're going to sample a 2x8 box from the set symbol
-symbolPixels = (233, 213, 241, 214)
+symbolPixels = (231, 222, 238, 224)
 
 highVariance = 10
 
 def scrapeCards():
-    page = requests.get('http://magic.wizards.com/en/articles/archive/card-image-gallery/eternal-masters')
+    page = requests.get('http://magic.wizards.com/en/articles/archive/card-image-gallery/modern-masters-2017-edition')
     tree = html.fromstring(page.content)
 
     cards = []
@@ -36,9 +39,10 @@ def scrapeCards():
         cardElements = child.xpath('//*/p/img')
         for cardElement in cardElements:
             card = {
-                "name": cardElement.attrib['alt'],
+                "name": cardElement.attrib['alt'].replace(u"\u2019",'\''),
                 "img": cardElement.attrib['src']
             }
+            card["url"] = card["img"]
             cards.append(card)
 
     return cards
@@ -59,12 +63,13 @@ def downloadImages(cards):
 
 def getRarity(cards):
     colorAverages = {
-        "Common": [10, 10, 10],
-        "Uncommon": [176, 212, 224],
-        "Rare": [217, 195, 130],
-        "Mythic Rare": [242, 142, 20]
+        "Common": [11, 7, 8],
+        "Uncommon": [175, 211, 223],
+        "Rare": [212, 188, 119],
+        "Mythic Rare": [235, 130, 22]
     }
     for card in cards:
+        #card['name'] = 'Griselbrand'
         cardImage = Image.open('images/' + setname + '/' + card['name'] + '.png')
         setSymbol = cardImage.crop(symbolPixels)
         cardHistogram = setSymbol.histogram()
@@ -85,11 +90,60 @@ def getRarity(cards):
                 card['rarity'] = color
         if variance > highVariance:
             # if a card isn't close to any of the colors, it's probably a planeswalker? make it mythic.
-            print card['name'], ' has high variance of ', variance, ', closest rarity is ', card['rarity']
+            print card['name'], 'has high variance of', variance, ', closest rarity is', card['rarity']
             card['rarity'] = "Mythic Rare"
-            # print card['name'], reds, greens, blues
-            # setSymbol.save('images/' + card['name'] + '.symbol.jpg')
+            #print card['name'], '$', reds, greens, blues
+        #setSymbol.save('images/' + card['name'] + '.symbol.jpg')
     return cards
+
+def concatmtgjson(mastersCards):
+    mastersSet = {
+        "name": "Modern Masters 2017 Edition",
+        "code": "MM3",
+        "releaseDate": "2017-03-17",
+        "border": "black",
+        "type": "reprint",
+        "booster": [["rare","mythic rare"],"uncommon","uncommon","uncommon","common","common","common","common","common","common","common","common","common","common",["foil mythic rare","foil rare","foil uncommon","foil common"]],
+        "cards": []
+    }
+    manualCards = {}
+    for card in manualCards:
+        scraped = False
+        for scrapedCard in mastersCards:
+            if scrapedCard['name'] == card:
+                scraped = True
+                print 'Using scraped card for ' + str(card)
+        if scraped == False:
+            mastersCards.append({
+                "name": card,
+                "rarity": manualCards[card]
+            })
+    with open('AllCards.json') as data_file:
+        allCards = json.load(data_file)
+    with open('AllSets.json') as data_file:
+        mtgjson = json.load(data_file)
+    for card in mastersCards:
+        allCard = allCards[card['name']]
+        #
+        if card.has_key('url'):
+            allCard['url'] = card['url']
+        else:
+            cardfound = False
+            for set in mtgjson:
+                if (cardfound == False):
+                    for setcard in mtgjson[set]['cards']:
+                        if setcard['name'] == card['name']:
+                            if setcard.has_key('multiverseid'):
+                                allCard['url'] = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + unicode(setcard['multiverseid']) + "&type=card"
+                                cardfound = True
+                                continue
+
+        allCard['rarity'] = card['rarity']
+        mastersSet['cards'].append(allCard)
+    mtgjson['MM3'] = mastersSet
+    print mastersSet
+    with open('AllSets.plus' + setname + '.json', 'w') as outfile:
+        json.dump(mtgjson, outfile, sort_keys=True, indent=2, separators=(',', ': '))
 
 if __name__ == '__main__':
     cards = scrapeCards()
@@ -98,3 +152,4 @@ if __name__ == '__main__':
     print cards
     with open(setname + '.json', 'w') as outfile:
         json.dump(cards, outfile, sort_keys=True, indent=2, separators=(',', ': '))
+    concatmtgjson(cards)
