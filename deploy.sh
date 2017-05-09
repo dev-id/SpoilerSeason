cde824d3952f1ba483db3c08c58383c3c4b2751a
tritoch <tritochs@gmail.com>
2017-05-09 10:27:58 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,16 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="files"
 
-function doCompile {
-  python main.py
-}
+#function doCompile {
+python main.py
+#}
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
-fi
+#if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+#    echo "Skipping deploy; just doing a build."
+#    doCompile
+#    exit 0
+#fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
